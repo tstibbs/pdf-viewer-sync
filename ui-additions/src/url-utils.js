6d02912195d4c82c99bddff1c3163a93fc89c705
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-03-17 21:18:02 +0000

    Adding ability to share file URL

--- ui-additions/src/url-utils.js
+++ ui-additions/src/url-utils.js
@@ -2,20 +2,21 @@ import {joinTokenParam, webSocketParam} from './constants.js'
 
 export class UrlUtils {
 	constructor() {
-		const urlParams = new URLSearchParams(location.search)
-		this._joinToken = urlParams.get(joinTokenParam)
-		this._webSocketBase = urlParams.get(webSocketParam)
+		this._urlParams = new URLSearchParams(location.search)
+		this._joinToken = this._urlParams.get(joinTokenParam)
+		this._webSocketBase = this._urlParams.get(webSocketParam)
 		this._viewerBase = new URL(location.pathname, location.href).href
 	}
 
 	updateJoinToken(joinToken) {
 		this._joinToken = joinToken
+		this._urlParams.set(joinTokenParam, this._joinToken)
 		const url = this.generateUrl()
 		window.history.pushState({}, '', url);
 	}
 
 	generateUrl() {
-		return `${this._viewerBase}?${webSocketParam}=${this._webSocketBase}&${joinTokenParam}=${this._joinToken}`
+		return `${this._viewerBase}?${this._urlParams}`
 	}
 
 	getJoinToken() {
