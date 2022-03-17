063c1fe08cb5bbdff6a9f3957fa2f5252f629e66
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-03-17 21:17:39 +0000

    Allowing cross-origin file loads

--- ui-additions/webpack.config.js
+++ ui-additions/webpack.config.js
@@ -1,7 +1,18 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
+
+//the viewer doesn't by default allow cross-origin pdf loads - this change undoes that restriction
+const hostedOriginReplacer = (content, absoluteFrom) => {
+	const from = /if \(HOSTED_VIEWER_ORIGINS\.includes\(viewerOrigin\)\) \{/i
+	const to = 'if (true /*HOSTED_VIEWER_ORIGINS.includes(viewerOrigin)*/ /* we want to allow pdfs from anywhere */) {'
+	if (absoluteFrom.endsWith('web/viewer.js') || absoluteFrom.endsWith('web/viewer.js.map')) {
+		return content.toString().replace(from, to)
+	} else {
+		return content
+	}
+}
 
 module.exports = {
 	mode: 'development',
@@ -17,6 +28,9 @@ module.exports = {
 					to: "..",
 					globOptions: {
 						ignore: ["**/viewer.html"],
+					},
+					transform: {
+						transformer: hostedOriginReplacer,
 					},
 				},
 			],
