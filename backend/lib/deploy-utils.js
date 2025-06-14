962f230405e07057be2e94b78a2492259d9e7146
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-06-14 17:48:32 +0100

    Node 22 upgrade

--- backend/lib/deploy-utils.js
+++ backend/lib/deploy-utils.js
@@ -14,7 +14,7 @@ export function buildGenericHandler(stack, name, entry, envs) {
 		entry: `src/handlers/${entry}.js`,
 		memorySize: 128,
 		timeout: Duration.seconds(20),
-		runtime: Runtime.NODEJS_20_X,
+		runtime: Runtime.NODEJS_22_X,
 		environment: envs
 	})
 	return handler
