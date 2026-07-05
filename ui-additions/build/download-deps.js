7badf122d4a4b3cfae88ee9958bdcc94a82fe016
Tim Stibbs <tstibbs@users.noreply.github.com>
2026-07-05 20:32:32 +0000

    moving to pnpm: ui

--- ui-additions/build/download-deps.js
+++ ui-additions/build/download-deps.js
@@ -4,14 +4,16 @@ import assert from 'assert/strict'
 
 /* Download the pdfjs release - because there isn't a module published to npm which contains all the files we need. The github release package appears to be the only option. */
 
-let response = await exec(`npm ls pdfjs-dist --json`)
+let response = await exec(`pnpm ls pdfjs-dist --json`)
 if (response.stderr) {
 	console.error(stderr)
 }
+console.log(response.stdout)
 let output = JSON.parse(response.stdout)
-assert.equal(Object.entries(output.dependencies).length, 1)
-assert.ok('pdfjs-dist' in output.dependencies)
-let version = output.dependencies['pdfjs-dist'].version
+const deps = output[0].dependencies
+assert.equal(Object.entries(deps).length, 1)
+assert.ok('pdfjs-dist' in deps)
+let version = deps['pdfjs-dist'].version
 console.log(`Downloading pdfjs@${version}`)
 await exec(`curl -L https://github.com/mozilla/pdf.js/releases/download/v${version}/pdfjs-${version}-dist.zip -o dist.zip && \
 rm -rf public/* && \
